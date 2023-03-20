0c3e65f80e8db452807aae4121da91e086245a93
Emmakaoba <emmakaoba@gmail.com>
2023-03-20 16:09:05 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,19 +11,17 @@
 	 * Description: Longer description
 	 */
 
-	struct dog(char, float)
+	struct dog
 	{
 		char *name;
 		float age;
 		char *owner;
 	};
 
-
 	/**
 	 * dog_t - typedef for struct dog
 	 */
 	typedef struct dog dog_t;
-
 
 	void init_dog(struct dog *d, char *name, float age, char *owner);
 	void print_dog(struct dog *d);
@@ -32,6 +30,4 @@
 	char *_strcpy(char *dest, char *src);
 	int _strlen(char *s);
 
-
 	#endif
-
